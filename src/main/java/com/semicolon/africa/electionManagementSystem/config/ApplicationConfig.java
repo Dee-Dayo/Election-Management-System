493c3b97b5dbb3b3423f6a2a769ccc842fcd02fd
philip <ajibolaphilip10@gmail.com>
2024-06-19 10:38:51 +0100

    pushing my task to my branch for review

--- src/main/java/com/semicolon/africa/electionManagementSystem/config/ApplicationConfig.java
+++ src/main/java/com/semicolon/africa/electionManagementSystem/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.semicolon.africa.electionManagementSystem.config;
 
 
+import com.semicolon.africa.electionManagementSystem.services.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-    private final UserDetailsService service;
+    private final UserDetailsServiceImpl service;
     @Bean
     public UserDetailsService userDetailsService(){
         return username -> service.loadUserByUsername(username);
--- src/main/java/com/semicolon/africa/electionManagementSystem/services/UserDetailsServiceImpl.java
+++ src/main/java/com/semicolon/africa/electionManagementSystem/services/UserDetailsServiceImpl.java
@@ -3,14 +3,14 @@ package com.semicolon.africa.electionManagementSystem.services;
 import com.semicolon.africa.electionManagementSystem.exceptions.UserNotFoundException;
 import com.semicolon.africa.electionManagementSystem.repositories.AdminRepository;
 import com.semicolon.africa.electionManagementSystem.repositories.VoterRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl  implements UserDetailsService {
     private final VoterRepository voters;
     private final AdminRepository admins;

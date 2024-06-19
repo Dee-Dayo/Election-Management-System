245551bf783d65eae41197c461164182eef708ae
Muhammad Baba Muhammad <babamu09@gmail.com>
2024-06-19 12:55:56 +0100

    Configured Database Setup

---
+++ src/test/java/com/semicolon/africa/electionManagementSystem/services/ElectionManagementTest.java
@@ -0,0 +1,15 @@
+package com.semicolon.africa.electionManagementSystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ElectionManagementTest {
+    @Autowired
+    private AdminService adminService;
+
+
+
+}

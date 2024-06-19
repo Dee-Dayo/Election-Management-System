87cfda357e15625315d9da2532c304d78d45cfdd
Ara <aramideotenaike@gmail.com>
2024-06-19 11:25:48 +0100

    added valid email to db

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -12,7 +12,7 @@ insert into candidates(candidate_id, first_name, last_name, username, email, pas
                        party_affiliation, election_id, time_created) values
 (100, 'Adewale', 'Adebowale', 'A2', 'Ajibolaphilip10@gmail.com', 'password', 'NATIONAL', 'PDP', 200,
  '2024-06-19T09:52:36.836436300'),
-(101, 'Olatunbosun', 'Adeotun', 'Bosun', 'Ajibolaphilp@gmail.com', 'password', 'NATIONAL', 'APC', 200,
+(101, 'Olatunbosun', 'Adeotun', 'Bosun', 'praiseoyewole560@gmail.com', 'password', 'NATIONAL', 'APC', 200,
  '2024-06-19T09:52:36.836436300'),
 (102, 'Gbenga', 'Akeem', 'Gbenga', 'victormsonter@gmail.com', 'password', 'NATIONAL', 'LP', 200,
  '2024-06-19T09:52:36.836436300'),
@@ -26,7 +26,7 @@ values
 (200, 'Rebecca', 'Nelson', '20-05-1999', 'Lagos', 'Darhyor2050@gmail.com', 'password',
  '2024-06-19T09:52:36.836436300'),
 (201, 'Bankole', 'Taiwo', '20-05-2000', 'Ogun', 'babsmoh12@gmail.com', 'password', '2024-06-19T09:52:36.836436300'),
-(202, 'Rukkayah', 'Akeem', '20-05-1970', 'Delta', 'babsmoh12@gmail.com', 'password', '2024-06-19T09:52:36.836436300'),
+(202, 'Rukkayah', 'Akeem', '20-05-1970', 'Delta', 'Josephfeyishetan@gmail.com', 'password', '2024-06-19T09:52:36.836436300'),
 (203, 'Faith', 'Joseph', '20-05-1991', 'Osun', 'm.yaro@native.semicolon.africa', 'password', '2024-06-19T09:52:36.836436300');
 
 
